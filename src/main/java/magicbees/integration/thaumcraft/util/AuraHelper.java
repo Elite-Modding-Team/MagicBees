e7da86d13df91050b12ba69d524a7f81d345229b
a <EYCHFSJD@gmail.com>
2025-04-07 20:24:30 +0800

    try to fix a strange bug...

--- src/main/java/magicbees/integration/thaumcraft/util/AuraHelper.java
+++ src/main/java/magicbees/integration/thaumcraft/util/AuraHelper.java
@@ -20,16 +20,18 @@ public class AuraHelper {
     public static void handleRandomChunk(IBeeGenome genome, IBeeHousing housing, Consumer<AuraChunk> handler) {
         World world = housing.getWorldObj();
         Random random = world.rand;
-        IBeeModifier modifier = BeeManager.beeRoot.createBeeHousingModifier(housing);
-        Vec3d range = new Vec3d(genome.getTerritory()).scale(modifier.getTerritoryModifier(genome, 1));
-        range.scale(1.5);
-        double r1 = random.nextGaussian(), r2 = random.nextGaussian();
-        if (r1 <= 1 && r2 <= 1 && r1 >= -1 && r2 >= -1) {
-            int x = ((int) (housing.getCoordinates().getX() + r1 * range.x)) >> 4;
-            int z = ((int) (housing.getCoordinates().getZ() + r2 * range.z)) >> 4;
-            AuraChunk auraChunk = AuraHandler.getAuraChunk(world.provider.getDimension(), x, z);
-            handler.accept(auraChunk);
-            world.getChunk(x, z).markDirty();
+        if (BeeManager.beeRoot != null) {
+            IBeeModifier modifier = BeeManager.beeRoot.createBeeHousingModifier(housing);
+            Vec3d range = new Vec3d(genome.getTerritory()).scale(modifier.getTerritoryModifier(genome, 1));
+            range.scale(1.5);
+            double r1 = random.nextGaussian(), r2 = random.nextGaussian();
+            if (r1 <= 1 && r2 <= 1 && r1 >= -1 && r2 >= -1) {
+                int x = ((int) (housing.getCoordinates().getX() + r1 * range.x)) >> 4;
+                int z = ((int) (housing.getCoordinates().getZ() + r2 * range.z)) >> 4;
+                AuraChunk auraChunk = AuraHandler.getAuraChunk(world.provider.getDimension(), x, z);
+                handler.accept(auraChunk);
+                world.getChunk(x, z).markDirty();
+            }
         }
     }
 
